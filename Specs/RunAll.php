4739df3b3486863acd3c11f48837e5f074d1e0ef
Christoph Pojer <christoph.pojer@gmail.com>
2010-05-15 16:31:58 +0200

    * Specs runner magic

--- Specs/RunAll.php
+++ Specs/RunAll.php
@@ -5,39 +5,53 @@ error_reporting(E_ALL | E_STRICT);
 require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
-require_once '../Source/Type.php';
-require_once '../Source/Iterable.php';
-require_once '../Source/ArrayObject.php';
-require_once '../Source/Array2.php';
-require_once '../Source/String.php';
+$namespace = trim(file_get_contents(__DIR__ . '/../namespace'));
 
-require_once './Type.php';
-require_once './Iterable.php';
-require_once './ArrayObject.php';
-require_once './Array2.php';
-require_once './String.php';
-
-class RunAll {
+spl_autoload_register(function($className) use ($namespace){
+	$className = explode('\\', $className);
+	if ($className[0] == $namespace) array_shift($className);
 	
-	public static function run(){
-		\PHPUnit_TextUI_TestRunner::run(self::suite());
+	$file = __DIR__ . '/../Source/' . implode('\\', $className) . '.php';
+	if (file_exists($file)) require_once $file;
+});
+
+$$namespace = function() use ($namespace){
+	$tests = array();
+	foreach (glob(__DIR__ . '/*.php') as $file){
+		$name = basename($file, '.php');
+		if ($name == 'RunAll') continue;
+		
+		$tests[] = $name;
+		require_once $file;
 	}
 	
-	public static function suite(){
-		$suite = new \PHPUnit_Framework_TestSuite('Type Tests');
-		$suite->addTestSuite('Tests\Type\TypeTest');
-		$suite->addTestSuite('Tests\Type\IterableTest');
-		$suite->addTestSuite('Tests\Type\ArrayObjectTest');
-		$suite->addTestSuite('Tests\Type\Array2Test');
-		$suite->addTestSuite('Tests\Type\StringTest');
+	return $tests;
+};
 
-		return $suite;
+if (!class_exists('RunAll')){
+	class RunAll {
+		
+		public static function run(){
+			\PHPUnit_TextUI_TestRunner::run(self::suite());
+		}
+		
+		public static function suite(){
+			$namespace = trim(file_get_contents(__DIR__ . '/../namespace'));
+			
+			$suite = new \PHPUnit_Framework_TestSuite($namespace . ' Tests');
+			
+			global $$namespace;
+			foreach ($$namespace() as $testCase)
+				$suite->addTestSuite('Tests\\' . $namespace . '\\' . $testCase . 'Test');
+
+			return $suite;
+		}
+		
 	}
-	
 }
 
 if (!defined('PHPUnit_MAIN_METHOD'))
-	define('PHPUnit_MAIN_METHOD', 'RunAll::run');
+	define('PHPUnit_MAIN_METHOD', $namespace);
 
-if (PHPUnit_MAIN_METHOD == 'RunAll::run')
+if (PHPUnit_MAIN_METHOD == $namespace)
 	RunAll::run();
